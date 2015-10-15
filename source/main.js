cf183b20a08fab66fd663eec294e3513b3c259cd
tomgp <tom.g.pearson@gmail.com>
2015-10-15 16:47:34 +0100

    Add sayHello method

--- source/main.js
+++ source/main.js
@@ -6,3 +6,7 @@ document.addEventListener('DOMContentLoaded', main);
 function main(){
 	console.log('Hey! Ho! Let\'s go!'); //remove this line if you don't care about The Ramones
 }
+
+function sayHello(name){
+	console.log('hello ' + name);
+}
