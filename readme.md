1caeb598dca42833e8761a461156632c76d33ab2
tomgp <tom.g.pearson@gmail.com>
2015-10-15 16:46:22 +0100

    Add DOM content loaded listener

---
+++ readme.md
@@ -0,0 +1,1 @@
+First exercise for a D3 workshop running at the Financial Times
---
+++ source/main.js
@@ -0,0 +1,8 @@
+'use strict';
+require('babelify/polyfill'); //remove this line if you don't care about ES6 pollyfils
+
+document.addEventListener('DOMContentLoaded', main);
+
+function main(){
+	console.log('Hey! Ho! Let\'s go!'); //remove this line if you don't care about The Ramones
+}
